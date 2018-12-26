d822292ee9f001b03f9ec2886c821a4f8ff62761
fdelia <fabio_d@gmx.net>
2018-12-26 14:21:22 +0100

    a

--- client/main.js
+++ client/main.js
@@ -19,7 +19,9 @@ Template.knagToKnag.onCreated(function(){
 });
 
 function getAllNodes(){
-
+    let allNodes = Edges.find().fetch().map(e => e.nodes);
+    let allNodesFlat = [].concat.apply([], allNodes).filter(x => x !== undefined);
+    return [...new Set(allNodesFlat)].sort();
 }
 
 function getNodesFrom(fromNode){
@@ -45,23 +47,17 @@ function getNodesBtn(fromNode, toNode){
         }}
     })*/
 
-    console.log(edges)
     var results = new Set();
     for (let i = 0; i < edges.length; ++i){
         let edge = edges[i];
 
-        // for "fromNode", check if can find other nodes connected to "toNode"
-        let otherNode = edge.nodes.find((node) => node !== fromNode);
-        let otherEdgeToOk = edges.find((e2) => e2.nodes.includes(otherNode) && e2.nodes.includes(toNode));
-        if (otherEdgeToOk){
-            results.add(otherNode);
-            continue;
-        }
+        let otherNode = edge.nodes.find((node) => node !== fromNode && node !== toNode);
+        //if (otherNode == "22") debugger;
 
-        // for "toNode"
-        otherNode = edge.nodes.find((node) => node !== toNode);
-        otherEdgeToOk = edges.find((e2) => e2.nodes.includes(otherNode) && e2.nodes.includes(fromNode));
-        if (otherEdgeToOk){
+        let otherEdgeToOkFrom = edges.some((e2) => e2.nodes.includes(otherNode) && e2.nodes.includes(fromNode));
+        let otherEdgeToOkTo = edges.some((e2) => e2.nodes.includes(otherNode) && e2.nodes.includes(toNode));
+
+        if (otherEdgeToOkFrom && otherEdgeToOkTo){
             results.add(otherNode);
             continue;
         }
@@ -70,33 +66,12 @@ function getNodesBtn(fromNode, toNode){
     return [...results];
 }
 
-function getNodes(fromNode, toNode = ""){
-    var edges = Edges.find({
-        $or: [
-            {f: fromNode},
-            {f: toNode},
-            {t: fromNode},
-            {t: toNode}
-        ]
-    }); //getEdges(fromNode, toNode);
-
-    var nodeNames = new Set()
-    edges.forEach((edge) => {
-
-    });
-
-
-    
-    edges.forEach((doc) => {
-        nodeNames.add(doc.f);
-        nodeNames.add(doc.t);
-    });
-    return [...nodeNames];
-}
-
 Template.knagToKnag.helpers({
     relatedNodes(){
         return getNodesBtn(fromNode, toNode);
+    },
+    allNodes(){
+        return getAllNodes();
     },
 
     fromNode(){
@@ -132,12 +107,23 @@ Template.knagToKnag.events({
         let val = document.getElementById('newKnag').value;
         if (val === "") return;
 
-        Edges.insert({
+        // insert first edge if not already exists
+        let alreadyExists = Edges.find({
             nodes: [fromNode, val]
-        });
-        Edges.insert({
+        }).count();
+        if (alreadyExists === 0)
+            Edges.insert({
+                nodes: [fromNode, val]
+            });
+
+        // insert second edge if not already exists
+        let alreadyExists2 = Edges.find({
             nodes: [val, toNode]
-        });
+        }).count();
+        if (alreadyExists2 === 0)
+            Edges.insert({
+                nodes: [val, toNode]
+            });
     },
 
     'submit .showKnags': function(e){
